e3d5d2f51160a551aa7b4b09cda79cf1488958a8
fatima <ibarrafatima692@gmail.com>
2026-02-11 15:19:48 -0600

    Agregar llamada funciones

--- main.jl
+++ main.jl
@@ -1,1 +1,15 @@
 println("Hola, Mundo desde Julia!")
+
+
+# 3. Funcion con operaciones
+function suma_cuadrados(a , b )
+  resultado = a ^2 + b ^2
+  return resultado
+# 2. Definicion de una funcion simple
+function saludar(nombre)
+  return "Hola, $nombre, bienvenido a Ciencia de Datos ."
+end
+
+# Llamada a funciones
+msg = saludar("Estudiante")
+valor = suma_cuadrados(3 , 4)
